2b6ffe55d0dbe74c707cf8a08a7641871d3e7895
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-12-01 12:45:10 -0500

    Remove unused CollectInfo object instantiation
    Removed instantiation of CollectInfo object.

--- java.java
+++ java.java
@@ -4,7 +4,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        // Create a new CollectInfo object to start the program
-        CollectInfo allinfo = new CollectInfo();
+        
     }
 }
